c2c4568fce3813eba6525ac110f9680ae8138ead
tarbiarswat <tarbiar.swat@northsouth.edu>
2021-04-18 03:21:18 +0600

    header added with css and bootstrap

---
+++ includes/head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+    <head>
+     
+        <meta charset="utf-8">
+       
+        <title>Bookflare - A Modern Education & LMS html template</title>
+
+        <meta name="author" content="ultraverge.com">
+
+        <!-- Mobile Specific Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <!-- Bootstrap  -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.css" >
+
+        <!-- Page Style -->
+        <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+        <link rel="stylesheet" type="text/css" href="stylesheets/responsive.css">
+
+
+        <link rel="shortcut icon" href="images/favicon.png">
+        <link rel="apple-touch-icon-precomposed" href="images/favicon-apple.png">
+
+
+    </head>  
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+    <head>
+
+    <?php  include('includes/head.php')  ?>
+    
+    </head>  
+    <body class="bg-body3"> 
+        <div class="boxed">
+
+            <?php  include('includes/header.php')  ?>
+            
+        </div>
+    </body>
+</html>
