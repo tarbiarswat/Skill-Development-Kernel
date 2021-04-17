a5ba92b66f48412291ec6935a1f1a344db53e72b
tarbiarswat <tarbiar.swat@northsouth.edu>
2021-04-18 03:38:31 +0600

    header.php customized

--- includes/header.php
+++ includes/header.php
@@ -2,60 +2,28 @@
                 <div class="container">
                     <div class="header-wrap clearfix">
                         <div id="logo">
-                            <a href="index.html"><img src="images/logo/logo.png" alt="bookflare" width="157" height="29" data-retina="images/logo/logo@2x.png" data-width="157" data-height="29"></a>
+                            <a href="index.php"></a>
                         </div>
                         <div class="nav-wrap flat-text-right style1">
                             <nav id="main-nav">
                                 <ul class="menu" >
+
                                     <li class="active">
-                                        <a href="#">HOME</a>
-                                        <ul class="submenu">
-                                            <li class="active"><a href="index.html">HOME 1</a></li>
-                                            <li><a href="index2.html">HOME 2</a></li> 
-                                            <li><a href="index3.html">HOME 3</a></li> 
-                                            <li><a href="index4.html">HOME 4</a></li>
-                                            <li><a href="index-animate.html">HOME ANIMATE</a></li> 
-                                        </ul>
+                                        <li class="active"><a href="#">HOME</a></li>
                                     </li>
+
                                     <li>
-                                        <a href="#">COURSES</a>
-                                        <ul class="submenu">
-                                            <li><a href="courses-grid-v1.html">COURSES V1</a></li>
-                                            <li><a href="courses-grid-v2.html">COURSES V2</a></li>
-                                            <li><a href="courses-grid-v3.html">COURSES V3</a></li>
-                                            <li><a href="courses-single.html">COURSES SINGLE</a></li>
-                                        </ul>
+                                        <li ><a href="#">PROGRAMS</a></li>
                                     </li>
-                                    <li><a href="instructors.html">INSTRUCTORS</a></li>
+
                                     <li>
-                                        <a href="#">EVENTS</a>
-                                        <ul class="submenu">
-                                            <li><a href="event-v1.html">EVENT V1</a></li>
-                                            <li><a href="event-v2.html">EVENT V2</a></li> 
-                                            <li><a href="event-single.html">EVENT SINGLE</a></li>
-                                        </ul>
+                                        <li ><a href="#">INTERNSHIPS</a></li>
                                     </li>
-                                    <li >
-                                        <a href="#">PAGES</a>
-                                        <ul class="submenu">
-                                            <li><a href="FAQs.html">FAQs</a></li>
-                                            <li><a href="about-us.html">ABOUT US</a></li>
-                                            <li class="item-has-child">
-                                                <a href="#">SHOP</a>
-                                                <ul class="submenu">
-                                                    <li ><a href="shop.html">SHOP</a></li>
-                                                    <li><a href="shop-single.html">SHOP SINGLE</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
+
                                     <li>
-                                        <a href="#">BLOG</a>
-                                        <ul class="submenu">
-                                            <li><a href="blog.html">BLOG</a></li>
-                                            <li><a href="blog-single.html">BLOG SINGLE</a></li>
-                                        </ul>
-                                    </li>                                             
+                                        <li ><a href="#">CONTACT US</a></li>
+                                    </li>
+                                                                          
                                 </ul>
                             </nav>
                         </div>
@@ -64,25 +32,12 @@
                             <div class="wrap-search-header">
                                 <div class="search-header">
                                     <form>
-                                        <input type="search" name="key" placeholder="Search for Courses">
+                                        <input type="search" name="key" placeholder="Search for Programs">
                                         <button class="btn-search"><span class="icon-search"></span></button>
                                     </form>
                                 </div>
                             </div>
-                            <div class="cart nav-top-cart-wrapper">
-                                <a href="#"><span class="bf-icon icon-cart"></span></a> <span class="count-cart">0</span>
-                                <div class="nav-shop-cart">
-                                    <div class="widget_shopping_cart_content">
-                                        <div class="woocommerce-min-cart-wrap">
-                                            <ul class="woocommerce-mini-cart cart_list product_list_widget flat-text-center">
-                                                <li class="woocommerce-mini-cart-item mini_cart_item">
-                                                    <span>No Items in Shopping Cart</span>
-                                                </li>
-                                            </ul>
-                                        </div><!-- /.widget_shopping_cart_content -->
-                                    </div>
-                                </div>
-                            </div>   
+                            
                         </div>
                         <div class="mobile-button">
                             <span></span>
