22acafe9066fd68e46833bb56e5067a29e98f0ee
tarbiarswat <tarbiar.swat@northsouth.edu>
2021-04-18 19:18:45 +0600

    merged

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,9 @@
                     <div class="row">
                         <div class="  col-md-6">
                             <div class="copyright flat-text-left">
+
+
+                            
                                 <p>© Copyright 2021 <a href="#">Skill Development Kernel</a>. All Rights Reserved.</p>
                             </div>
                         </div>
@@ -18,10 +21,12 @@
                         </div>
                     </div>
                 </div>
-            </div> <!-- /.bottom -->
+</div> <!-- /.bottom -->
             
 <a id="scroll-top"></a>
-<script src="javascript/jquery.min.js"></script>
+
+
+        <script src="javascript/jquery.min.js"></script>
         <script src="javascript/parallax.js"></script>
         <script src="javascript/owl.carousel.min.js"></script>
         <script src="javascript/jquery-fancybox.js"></script>
@@ -33,19 +38,3 @@
         <script src="javascript/smoothscroll.js"></script>
         
         <script src="javascript/main.js"></script>
-
-        <!-- Revolution Slider -->
-        <script src="revolution/js/jquery.themepunch.tools.min.js"></script>
-        <script src="revolution/js/jquery.themepunch.revolution.min.js"></script>
-        <script src="revolution/js/slider.js"></script>
-
-        <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->    
-        <script src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
-        <script src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
-        <script src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-        <script src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-        <script src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
-        <script src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-        <script src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-        <script src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-        <script src="revolution/js/extensions/revolution.extension.video.min.js"></script>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<?php  include('configs/dbconfig.php') ?>
+
 
     <head>
 
@@ -34,6 +36,44 @@
 
 
         </div>
-        <?php  include('includes/footer.php')  ?>
+        
+        <div class="bottom bg-15222e">
+                <div class="container">
+                    <div class="row">
+                        <div class="  col-md-6">
+                            <div class="copyright flat-text-left">
+
+                                <?php
+
+                                
+                                    
+                                    
+                                    
+                                    
+                                   
+
+    
+                                echo '<p> <a href="#">'.$value.'</a></p>';
+
+                                ?>
+
+                            
+                            </div>
+                        </div>
+                        <div class="  col-md-6">
+                            <div class="widget flat-text-right no-border">
+                                <ul class="list">
+                                    <li><a href="#">Privacy</a></li>
+                                    <li><a href="#">Terms</a></li>
+                                    <li><a href="#">Cookie Policy</a></li>
+                                    <li><a href="#">Sitemap</a></li>                    
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</div> <!-- /.bottom -->
+
+
     </body>
 </html>
